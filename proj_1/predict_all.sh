6656f27749163d3437b88b40d333129ce2fd45c3
David Zhang <zdavid0123@gmail.com>
2018-03-15 15:10:28 +0000

    Put all prediction results into one file.

--- proj_1/predict_all.sh
+++ proj_1/predict_all.sh
@@ -2,10 +2,19 @@
 export WORKON_HOME=$HOME/.virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 workon tensorflow
-for filename in training_geno/*training_geno.dat; do
-	fname=${filename#*/} 
+allPred=predicted_pheno/ALL_predicted_pheno.dat
+rm -f ${allPred}
+for train_path in training_geno/*training_geno.dat; do
 	date
-	echo Predicting from ${fname} ...
-	python predict.py ${fname}
+	echo Predicting from ${train_fname} ...
+	train_fname=${train_path#*/} 
+	gene=${train_fname%.*}
+	gene=${gene%.*}
+	pred_path=predicted_pheno/${gene}.predicted_pheno.dat
+	python predict.py ${train_fname}
+	echo -n ${gene} >> ${allPred}
+	echo -ne '\t' >> ${allPred}
+	cat ${pred_path} | awk 1 ORS='\t' >> ${allPred}
+	echo >> ${allPred}
 	echo Done!
 done
